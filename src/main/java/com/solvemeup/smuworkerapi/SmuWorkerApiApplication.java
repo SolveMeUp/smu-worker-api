13592347cd274da7f61108d036b3eca353ed631b
choijw1004 <97choijw@gmail.com>
2026-02-09 10:44:34 +0900

    init: project inint

---
+++ src/main/java/com/solvemeup/smuworkerapi/SmuWorkerApiApplication.java
@@ -0,0 +1,13 @@
+package com.solvemeup.smuworkerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmuWorkerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmuWorkerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solvemeup/smuworkerapi/SmuWorkerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvemeup.smuworkerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmuWorkerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
