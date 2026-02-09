b4bc2c4a7fedeb1da29fd0cc353da1380b438d75
choijw1004 <97choijw@gmail.com>
2026-02-09 17:15:44 +0900

    feat: enum 타입 JudgeResult 작성

---
+++ src/main/java/com/solvemeup/smuworkerapi/worker/enums/JudgeResult.java
@@ -0,0 +1,19 @@
+package com.solvemeup.smuworkerapi.worker.enums;
+
+/*
+    AC,    // Accepted
+    WA,    // Wrong Answer
+    TLE,   // Time Limit Exceeded
+    MLE,   // Memory Limit Exceeded
+    CE,    // Compile Error
+    RE,    // Runtime Error
+ */
+public enum JudgeResult {
+    AC,
+    WA,
+    TLE,
+    MLE,
+    CE,
+    RE,
+    SYSTEM_ERROR
+}
