794a09d8a09647250f6d0f7315c66c54183b9fdf
choijw1004 <97choijw@gmail.com>
2026-02-22 23:36:23 +0900

    fix: RabbitConfig 수정

--- src/main/java/com/solvemeup/smuworkerapi/worker/config/RabbitConfig.java
+++ src/main/java/com/solvemeup/smuworkerapi/worker/config/RabbitConfig.java
@@ -7,14 +7,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
 
-    public static final String JUDGE_EXCHANGE  = "judge.exchange";
-    public static final String REQUEST_ROUTING_KEY = "judge.request";
-    public static final String RESULT_ROUTING_KEY = "judge.result";
+    public static final String JUDGE_EXCHANGE = "judge.exchange";
+    public static final String JUDGE_REQUEST_ROUTING_KEY = "judge.request";
+    public static final String JUDGE_RESULT_ROUTING_KEY = "judge.result";
+    public static final String JUDGE_REQUEST_QUEUE = "judge.request.queue";
 
-    public static final String REQUEST_QUEUE = "judge.request.queue";
+    public static final String EXAMPLE_EXCHANGE = "example.exchange";
+    public static final String RUN_REQUEST_ROUTING_KEY = "example.run";
+    public static final String RUN_RESULT_ROUTING_KEY = "example.result";
+    public static final String RUN_REQUEST_QUEUE = "example.run.queue";
 
     @Bean
-    public JacksonJsonMessageConverter jacksonJsonMessageConverter(){
+    public JacksonJsonMessageConverter jacksonJsonMessageConverter() {
         return new JacksonJsonMessageConverter();
     }
 }
